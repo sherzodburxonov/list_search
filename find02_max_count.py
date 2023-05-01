2ed91f13368c981451babd936b4a0617ea99352b
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 16:20:28 +0500

    02

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,16 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    n=0
+    i=0
+    KATTASI=data[0]
+    while n<len(data):
+        if KATTASI<data[n]:
+            KATTASI=data[n]
+        n=n+1
+    while n<len(data): 
+        if KATTASI==data[n]:
+            i=i+1      
+        n=n+1
+    return i
+print(find_max_count([12, 2, 5, 2, 7, 12, 1]))
