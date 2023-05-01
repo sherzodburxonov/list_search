ebd23c9c15e88fe3a7f15d3fe5c2114d22920d82
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:26:25 +0500

    11

--- find11_min_odd.py
+++ find11_min_odd.py
@@ -5,5 +5,12 @@ def find_min_odd(data):
         data: list of numbers
     returns: minimum odd number in the list
     """
-    return 0
-
+    n=0
+    kichkinasi=data[0]
+    while n<len(data):
+        if data[n]%2==1:
+            if kichkinasi>data[n]:
+                kichkinasi=data[n]
+        n=n+1
+    return kichkinasi
+print(find_min_odd([12, 2, 5, 2, 7, 17, 1]))
