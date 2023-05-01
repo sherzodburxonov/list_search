d2d83850e82fbb36e89cd18220664c73a35b829d
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 16:48:14 +0500

    02

--- find02_max_count.py
+++ find02_max_count.py
@@ -12,9 +12,7 @@ def find_max_count(data):
         if KATTASI<data[n]:
             KATTASI=data[n]
         n=n+1
-    while n<len(data): 
-        if KATTASI==data[n]:
+        if KATTASI==data[n-1]:
             i=i+1      
-        n=n+1
     return i
-print(find_max_count([12, 2, 5, 2, 7, 12, 1]))
+print(find_max_count([12, 2, 12, 2, 7, 12, 12]))
