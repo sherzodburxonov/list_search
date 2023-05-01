ade1e3647df393fe58b587414c94b570ec66f056
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 16:49:20 +0500

    03

--- find03_max_min_sum.py
+++ find03_max_min_sum.py
@@ -5,4 +5,15 @@ def find_max_min_sum(data):
         data: list of numbers
     returns: sum of the maximum and minimum numbers in the list
     """
-    return 0
+    n=0
+    KATTASI=data[0]
+    kichkinasi=data[0]
+    while n<len(data):
+        if KATTASI<data[n]:
+            KATTASI=data[n]
+        if kichkinasi>data[n]:
+            kichkinasi=data[n]
+        n=n+1
+    return kichkinasi+KATTASI
+print(find_max_min_sum([12, 2, 5, 2, 7, 9, 1]))
+
