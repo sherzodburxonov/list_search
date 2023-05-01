6a743536ccb8aa74600050878dab0f0d30386fce
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 15:56:14 +0500

    01

--- find01_max.py
+++ find01_max.py
@@ -5,5 +5,12 @@ def find_max(data):
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
+    n=0
+    KATTASI=data[0]
+    while n<len(data):
+        if KATTASI<data[n]:
+            KATTASI=data[n]
+        n=n+1
+    return KATTASI
+print(find_max([12, 2, 5, 2, 7, 9, 1]))
     
