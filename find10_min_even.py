a9d38e64a7abf47e36a760bcd5b86dfb1c61459f
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:25:19 +0500

    10

--- find10_min_even.py
+++ find10_min_even.py
@@ -5,5 +5,14 @@ def find_min_even(data):
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    n=0
+    kichkinasi=data[0]
+    while n<len(data):
+        if data[n]%2==0:
+            if kichkinasi>data[n]:
+                kichkinasi=data[n]
+        n=n+1
+    return kichkinasi
+print(find_min_even([12, 2, 5, 2, 7, 17, 1]))
+
 
