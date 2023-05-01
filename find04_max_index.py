13448c3c4e5240c6cf39180261d86058b73123ae
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:21:43 +0500

    04

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,12 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    n=0
+    KATTASI=data[0]
+    while n<len(data):
+        if KATTASI<data[n]:
+            KATTASI=data[n]
+        n=n+1
+    return data.index(KATTASI)
+print(find_max_index([12, 2, 5, 2, 7, 9, 1]))
+    
