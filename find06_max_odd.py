5d4dd3e664927c26fe42023796c50f964ff44ec4
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:06:18 +0500

    06

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,12 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    n=0
+    KATTASI=data[0]
+    while n<len(data):
+        if data[n]%2==1:
+            if KATTASI<data[n]:
+                KATTASI=data[n]
+        n=n+1
+    return KATTASI
+print(find_max_odd([12, 2, 5, 2, 7, 17, 1]))
