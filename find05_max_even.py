975628be3808925901b436dc123810b1f705eeba
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:04:14 +0500

    05

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,4 +5,14 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    return 0
+    n=0
+    KATTASI=data[0]
+    while n<len(data):
+        if data[n]%2==10:
+            if KATTASI<data[n]:
+                KATTASI=data[n]
+        n=n+1
+    return KATTASI
+print(find_max_even([12, 2, 5, 2, 7, 17, 1]))
+    
+
