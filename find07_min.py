b70411977d8c4c4a87d9b3856d643292dc040014
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-05-01 17:08:52 +0500

    07

--- find07_min.py
+++ find07_min.py
@@ -5,5 +5,12 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
-
-    return 0
+    n=0
+    kichkinasi=data[0]
+    while n<len(data):
+        if kichkinasi>data[n]:
+            kichkinasi=data[n]
+        n=n+1
+    return kichkinasi
+print(find_min([12, 2, 5, 2, 7, 9, 1]))
+    
